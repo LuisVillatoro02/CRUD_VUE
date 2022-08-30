c5ba9dde3885c4b81d24be3bc325531854923e85
LuisVillatoro02 <lvillatorom4@miumg.edu.gt>
2022-08-30 17:40:46 -0600

    Se termina el codigo

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+}
---
+++ database/seeders/TaskTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Task;
+
+class TaskTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Task::class, 5)->create();
+    }
+}
